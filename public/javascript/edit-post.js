bccd080556fece505821ad96d67430c688d2d159
Lindsey Kelly <nightbirdphoto@gmail.com>
2022-07-26 14:27:44 -0400

    js files

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -1,14 +1,14 @@
-async function newFormHandler(event) {
+async function editFormHandler(event) {
     event.preventDefault();
   
-    const title = document.querySelector('input[name="post-title"]').value;
-    const post_url = document.querySelector('input[name="post-url"]').value;
-  
-    const response = await fetch(`/api/posts`, {
-      method: 'POST',
+    const title = document.querySelector('input[name="post-title"]').value.trim();
+    const id = window.location.toString().split('/')[
+      window.location.toString().split('/').length - 1
+    ];
+    const response = await fetch(`/api/posts/${id}`, {
+      method: 'PUT',
       body: JSON.stringify({
-        title,
-        post_url
+        title
       }),
       headers: {
         'Content-Type': 'application/json'
@@ -16,11 +16,11 @@ async function newFormHandler(event) {
     });
   
     if (response.ok) {
-      document.location.replace('/dashboard');
+      document.location.replace('/dashboard/');
     } else {
       alert(response.statusText);
     }
   }
   
-  document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
+  document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
   
